5fc9dbc77312b50dacab6400524a140ccbc19b4e
TKDKid1000 <rhoneeiler@gmail.com>
2023-02-13 16:51:12 -0800

    Temporary public worker.js

--- client/public/worker.js
+++ client/public/worker.js
@@ -9,12 +9,42 @@
 const ctx = self
 
 ctx.addEventListener("push", (event) => {
-    const payload = event.data?.text() ?? "no payload"
-    event.waitUntil(
-        ctx.registration.showNotification("Wyvern", {
-            body: payload
-        })
-    )
+    const payload = event.data?.json()
+    if (!payload) return
+    event.waitUntil(ctx.registration.showNotification("Wyvern", payload))
+})
+
+ctx.addEventListener("notificationclick", (event) => {
+    event.preventDefault()
+    event.notification.close()
+    switch (event.notification.data.type) {
+        case "MESSAGE_CREATE": {
+            event.waitUntil(
+                ctx.clients.matchAll({ type: "window" }).then((clients) => {
+                    if (clients.length === 0) {
+                        event.waitUntil(
+                            ctx.clients.openWindow(
+                                `/channels/${event.notification.data.guildId}/${event.notification.data.channelId}`
+                            )
+                        )
+                    } else {
+                        /** @type {WindowClient} */
+                        const client = clients[0]
+                        const regex = new RegExp(
+                            String.raw`^\/channels\/${event.notification.data.guildId}\/${event.notification.data.channelId}(?:\/${event.notification.data.messageId})?$`,
+                            "gm"
+                        )
+                        console.log({ url: client.url, regex })
+                        if (new URL(client.url).pathname.match(regex))
+                            event.waitUntil(client.focus())
+                        // client.
+                        // client.postMessage("MESSAGE_CREATE", "")
+                    }
+                })
+            )
+            break
+        }
+    }
 })
 
 // self.addEventListener("fetch", (event: FetchEvent) => {
